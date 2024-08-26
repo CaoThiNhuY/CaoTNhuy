a275ad552755521c494b9b5150679fb49e4446e3
CaoThiNhuY <nhuycao25@gmail.com>
2024-08-26 20:20:10 +0700

    Add files via upload

---
+++ maxmin.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 13:53:52 2024
+
+@author: Admin
+"""
+
+# Nhập ba số nguyên từ người dùng
+a = int(input("Nhập số nguyên thứ nhất: "))
+b = int(input("Nhập số nguyên thứ hai: "))
+c = int(input("Nhập số nguyên thứ ba: "))
+
+# Tìm số lớn nhất và nhỏ nhất
+max_so = max(a, b, c)
+min_so = min(a, b, c)
+
+# In kết quả
+print("Số lớn nhất là:", max_so)
+print("Số nhỏ nhất là:", min_so)
